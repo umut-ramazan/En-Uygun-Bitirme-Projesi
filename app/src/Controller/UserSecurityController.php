301d1e17d4c3952ab7d85bd0410ef226b5e275c3
Umut Ramazan <umutramazan26@gmail.com>
2022-08-10 17:20:33 +0300

    Symfony Security Update

--- app/src/Controller/UserSecurityController.php
+++ app/src/Controller/UserSecurityController.php
@@ -16,7 +16,14 @@ class UserSecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
          if ($this->getUser()) {
-             return $this->redirectToRoute('app_client');
+
+             if ($this->getUser()->getRoles() == 'ROLE_ADMIN') {
+                 return $this->redirectToRoute('app_panel');
+             }
+             else{
+                 return $this->redirectToRoute('app_client');
+             }
+
          }
 
         // get the login error if there is one
